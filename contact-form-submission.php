57fa28ca87f9970f60cc6a89f38765b2a09d1bf1
root <henrybond158@icloud.com>
2014-04-25 13:15:00 +0100

    new commit

--- contact-form-submission.php
+++ contact-form-submission.php
@@ -5,11 +5,11 @@ if (!isset($_POST['save']) || $_POST['save'] != 'contact') {
     header('Location: contact.php'); exit;
 }
 	
-// get the posted data
+// get the data
 $name = $_POST['contact_name'];
-$email_address = $_POST['contact_email'];
-$phone = $_POST['contact_phone'];
-$message = $_POST['contact_message'];
+$email = $_POST['contact_email'];
+$phoneNumber = $_POST['contact_phone'];
+$content = $_POST['contact_message'];
 	
 // check that a name was entered
 if (empty($name))
@@ -37,13 +37,13 @@ $headers .= "Reply-To: $email_address\r\n";
 
 // write the email content
 $email_content = "Name: $name\n";
-$email_content .= "Email Address: $email_address\n";
-$email_content .= "Phone Number: $phone\n";
-$email_content .= "Message:\n\n$message";
+$email_content .= "Email Address: $email\n";
+$email_content .= "Phone Number: $phoneNumber\n";
+$email_content .= "Message:\n\n$content";
 	
 // send the email
 //ENTER YOUR INFORMATION BELOW FOR THE FORM TO WORK!
-mail ('YOUR-EMAIL-ADDRESS@YOUR-DOMAIN.com', 'YOUR WEBSITE NAME - Contact Form Submission', $email_content, $headers);
+mail ($email, 'Your submission', $content, $headers);
 	
 // send the user back to the form
 header('Location: contact.html?s='.urlencode('Thank you for your message.')); exit;
